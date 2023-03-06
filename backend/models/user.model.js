a79d08a54fbbe0e728b2ed4a3a61d831b4390378
Averand1 <leonk8867@gmail.com>
2023-03-07 02:19:19 +0530

    User model

---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+const User = sequelize.define('user', {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
+  });
+
+module.exports = User; 
