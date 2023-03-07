240f1ccd5d2c6121941a721bb44c6535f0721d29
Averand1 <leonk8867@gmail.com>
2023-03-08 01:54:00 +0530

    entry point

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./backend/config/db.config');
+
+// connect to the database
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error:', err));
+
+const app = express();
+
+// enable CORS
+app.use(cors());
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.use('/api/user', require('./backend/routes/userRoutes'));
+
+// start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
