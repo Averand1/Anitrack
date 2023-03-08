f58b94ac0ee19b3dfd920cb43e2077092cfd6a01
Averand1 <leonk8867@gmail.com>
2023-03-09 01:48:55 +0530

    login API created

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,20 +1,53 @@
+const { validPassword } = require('../middleware/validPassword')
 const User = require('../models/user.model')
 
 exports.signup = (req, res) => {
-    const {username, email, password} = req.body
+    const { username, email } = req.body
+    const { salt, hash } = validPassword(req.body.password);
 
     const newUser = new User({
         username,
         email,
-        password
-    });
+        password: hash,
+        SALT: salt
+    })
 
     newUser.save()
-    .then(user => {
-        res.status(200).json({message: 'User Created Succesfully'})
-    })
-    .catch(err => {
-        err.status(500).json({message: 'User was not created, please check your information again'})
-    })
+        .then((user) => {
+            res.status(200).json({ message: 'User created successfully' })
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'User was not created, please check your information again' })
+        })
+}
 
+exports.login = (req, res) => {
+    let message = []
+    let success = false
+    let status = 404
+    User.findOne({
+        where: {
+            'email': req.body.email,
+        }
+    }).then(function (user) {
+        if (user) {
+            message.push('User found')
+            if (User.prototype.validPassword(req.body.password, user.SALT, user.password)) {
+                status = 200;
+                success = true;
+                message.push('You are authorized');
+            }  else {
+                message.push('Wrong Pass')
+            }
+        } else {
+            message.push('Check credentials')
+        }
+
+        res.json({
+            status,
+            success,
+            message
+        })
+    })
 }
