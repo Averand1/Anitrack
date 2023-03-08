5d08ef53acf545cd8a92ac01ec9cbfa6a8608a6b
Averand1 <leonk8867@gmail.com>
2023-03-09 01:47:54 +0530

    Old entry point

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-const axios = require('axios')
-const mysql = require('mysql2')
 const express = require('express')
 const app = express();
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const User = require('./backend/models/user.model')
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
 
 // API endpoint for anime data
-let url = "https://kitsu.io/api/edge/anime"
-
-// Function to establish a connection to the database
-// async function connectToDatabase() {
-//   try {
-//     const connection = await mysql.createConnection({
-//       'host':'//',
-//       'user':'//',
-//       'password':'//',
-//       'database': '//'
-//   });
-//     return connection
-
-//   } catch (error) {
-//     console.log('Failed to establish connection', error);
-//     return null;
-//   }
-// }
-
-
-// User.create({
-//   email: 'john3.doe@example.com',
-//   password: 'password123'
-// }).then(user => {
-//   console.log('New user created:');
-//   console.log(user);
-// }).catch(err => {
-//   console.error('Error creating user:', err);
-// });
-
 
 sequelize.sync({ force: false })
   .then(() => {
