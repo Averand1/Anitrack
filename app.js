8ad3559032b66f229646cecce909b644936b55db
Averand1 <leonk8867@gmail.com>
2023-03-10 16:21:26 +0530

    entry point

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const session = require('express-session')
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const sessionMiddleware =  require('./backend/middleware/session')
 const db = require('./backend/config/db.config');
 
 // connect to the database
@@ -8,7 +10,9 @@ db.authenticate()
   .then(() => console.log('Database connected...'))
   .catch(err => console.log('Error:', err));
 
-const app = module.exports = express();
+const app = express();
+
+app.use(sessionMiddleware)
 // enable CORS
 app.use(cors());
 
