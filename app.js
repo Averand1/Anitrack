2f49b532174f4e77676f1cf37238233871940288
Averand1 <leonk8867@gmail.com>
2023-03-11 17:11:07 +0530

    Entry point

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
-app.use('/api/user', require('./backend/routes/userRoutes'));
+app.use('/api/user', require('./backend/routes/userRoutes'), require('./backend/routes/animeRoutes'));
 
 // start the server
 const PORT = process.env.PORT || 5000;
