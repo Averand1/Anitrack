00f91c2aa80c8f29130cb483f3e0fbaf7e3da84d
Averand1 <leonk8867@gmail.com>
2023-03-11 21:07:12 +0530

    added paginaton logic

--- backend/controller/animeController.js
+++ backend/controller/animeController.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const router = express.Router();
 const Anime = require('../models/anime.model');
 
 exports.anime = async (req, res) => {
     try {
       if (req.session?.user) {
-        const animeList = await Anime.findAll({limit: 50});
+        const page = parseInt(req.query.page) || 1; 
+        const pageSize = parseInt(req.query.pageSize) || 10; 
+        const offset = (page - 1) * pageSize; 
+        const animeList = await Anime.findAll({ limit: pageSize, offset: offset })
         res.json(animeList);
       } else {
         res.status(403).json({ error: 'Forbidden' });
       }
     } catch (error) {
       console.error(error);
-      res.status(403).json({ error: 'Server error' });
+      res.status(500).json({ error: 'Server error' });
     }
 }
