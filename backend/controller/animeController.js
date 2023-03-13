a499c43bd85eb3bd4ba57c1744489e7738f5a778
Averand1 <leonk8867@gmail.com>
2023-03-13 21:01:18 +0530

    removed console logs

--- backend/controller/animeController.js
+++ backend/controller/animeController.js
@@ -12,7 +12,7 @@ exports.anime = async (req, res) => {
         res.status(403).json({ error: 'Forbidden' });
       }
     } catch (error) {
-      console.error(error);
+
       res.status(500).json({ error: 'Server error' });
     }
 }
