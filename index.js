233941aef14217bef8c83e8bc0e152b8ce58fef9
Averand1 <leonk8867@gmail.com>
2023-03-22 00:06:31 +0530

    entry point

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const app = express();
 
 app.use(sessionMiddleware)
 // enable CORS
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -26,12 +28,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
+app.use('/api', (req, res) => {
+  res.status(200).json({message:"API is running"})
+})
 app.use('/api', userRouter);
 app.use('/api', animeRouter);
 app.use('/api', profileRouter);
 app.use('/api', starredRouter);
 
 // start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
