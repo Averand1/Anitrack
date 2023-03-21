058be47e68c071ed08625d5435bcb14e88dfb69b
Averand1 <leonk8867@gmail.com>
2023-03-22 00:06:36 +0530

    db config

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config('../../.env')
 const Sequelize = require('sequelize');
-module.exports = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
-  host: process.env.RDS_HOST,
-  dialect: 'mysql'
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 });
