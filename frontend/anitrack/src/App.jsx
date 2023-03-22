44637a750389b366fc0967c0e29a03c593c19274
Averand1 <leonk8867@gmail.com>
2023-03-23 00:16:11 +0530

    Main App

--- frontend/anitrack/src/App.jsx
+++ frontend/anitrack/src/App.jsx
@@ -1,14 +1,46 @@
-import './App.css'
-import Signup from './Signup'
+import React, { useState } from 'react';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import '../src/App.css';
 
-function App() {
+const App = () => {
+  const [showSignup, setShowSignup] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showLogout, setShowLogout] = useState(false);
+
+  const handleSignupClick = () => {
+    setShowSignup(true);
+    setShowLogin(false);
+    setShowLogout(false);
+  };
+
+  const handleLoginClick = () => {
+    setShowSignup(false);
+    setShowLogin(true);
+    setShowLogout(false);
+  };
+
+  const handleLogoutClick = () => {
+    setShowSignup(false);
+    setShowLogin(false);
+    setShowLogout(true);
+  };
 
   return (
-    <div className="App">
-    <h1>My App</h1>
-    <Signup/>  
+    <div className="app-container">
+      <nav>
+        <button className="nav-button" onClick={handleSignupClick}>Signup</button>
+        <button className="nav-button" onClick={handleLoginClick}>Login</button>
+        <button className="nav-button" onClick={handleLogoutClick}>Logout</button>
+      </nav>
+      <div className="component-container">
+        {showSignup && <Signup />}
+        {showLogin && <Login />}
+        {showLogout && <Logout />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
